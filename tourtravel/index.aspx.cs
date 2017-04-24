e0fbcb3180fa6740cb956aa23f7bfef9685910f4
asminf205 <namlhps04772@fpt.edu.vn>
2017-04-24 13:59:52 +0700

    no message

---
+++ tourtravel/index.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class preview_dotnet_templates_newlook_index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+"dependencies": {
+...
+    "Microsoft.AspNet.Server.Kestrel": "1.0.0-beta8",
+},
+"commands": {
+...
+    "kestrel": "Microsoft.AspNet.Hosting --server Microsoft.AspNet.Server.Kestrel"
+},
+"frameworks": {
+    "dnxcore50": { }
+}
+    }
+}
